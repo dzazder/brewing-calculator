ac4fdd8fd1cd2c7da8cd3158c57ec91add76b10b
Bartosz Lampart <dzazder@gmail.com>
2016-09-30 22:48:10 +0200

    dynamically created layout

--- app/src/main/java/dev/lampart/bartosz/brewingcalculator/fragments/FragmentIBU.java
+++ app/src/main/java/dev/lampart/bartosz/brewingcalculator/fragments/FragmentIBU.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import dev.lampart.bartosz.brewingcalculator.R;
 
@@ -24,7 +26,30 @@ public class FragmentIBU extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ibu, container, false);
+        View view = inflater.inflate(R.layout.fragment_ibu, container, false);
+
+        Button btnAddHop = (Button)view.findViewById(R.id.btn_add_hop);
+        btnAddHop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return view;
+    }
+
+    private LinearLayout createHopLine() {
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        LinearLayout lay = new LinearLayout(getContext());
+        lay.setLayoutParams(lp);
+        lay.setOrientation(LinearLayout.HORIZONTAL);
+
+        
+
+
+        return lay;
     }
 
 }
